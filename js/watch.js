08f411df2490ed8450513f21ff5ea293a8d3e5ea
Nikita Tribunskiy <nekit2326@gmail.com>
2017-04-09 18:57:39 +0300

    frames

--- js/watch.js
+++ js/watch.js
@@ -1,4 +1,8 @@
-function Watch() {
+var p = document.createElement('p');
+p.className = 'hidden-xs';
+p.id='watch';
+
+setInterval(function () {
     var date = new Date();
     var hours = date.getHours();
     var minutes = date.getMinutes();
@@ -6,6 +10,6 @@ function Watch() {
     if (hours < 10) hours = "0" + hours;
     if (minutes < 10) minutes = "0" + minutes;
     if (seconds < 10) seconds = "0" + seconds;
-    document.getElementById("watch").innerHTML = hours + ":" + minutes + ":" + seconds;
-    setTimeout("Watch()", 1000);
-}
+    p.innerText = hours + ":" + minutes + ":" + seconds;
+    document.getElementById('navik').appendChild(p);
+}, 1000);
